7d1f92a00433d0cc1039649c048c99e30a355826
Mitchell Cohen <mitchell.cohen3@mail.mcgill.ca>
2025-08-26 16:02:28 -0400

    Add SO3 parameter block

---
+++ include/lib/SO3ParameterBlock.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "ParameterBlock.h"
+#include "lie/LieDirection.h"
+#include "lie/SO3.h"
+
+#include "local_parameterizations/SO3LocalParameterization.h"
+
+/**
+ * @brief Parameter block for SO(3) rotations.
+ *
+ * The global state is represented as a 9D vector, representing the flattened
+ * rotation matrix.
+ *
+ * The SO3LocalParameterization allows for the rotation to be updated using
+ * either a left or right perturbation model, written as C = Exp(delta_xi) *
+ * C_bar           (left perturbation) C = C_bar * Exp(delta_xi) (right
+ * perturbation)
+ */
+
+class SO3ParameterBlock : public ParameterBlockBase<9, 3> {
+public:
+  explicit SO3ParameterBlock(const std::string &name = "so3_param_block",
+                             LieDirection direction = LieDirection::left)
+      : ParameterBlock<9, 3>(name) {
+    local_parameterization_ptr_ = new SO3LocalParameterization(direction);
+  }
+
+  // Construct directly from a DCM
+  SO3ParameterBlock(const Eigen::Matrix3d &C,
+                    const std::string &name = "so3_param_block",
+                    LieDirection direction = LieDirection::left)
+      : SO3ParameterBlock(name, direction) {}
+
+  void setFromMatrix(const Eigen::Matrix3d &C) {
+    Eigen::Matrix<double, 9, 1> flattened_C = SO3::flatten(C);
+    this->setEstimate(Eigen::VectorXd(flattened_C));
+  }
+
+  Eigen::Matrix3d attitude() const {
+    Eigen::Map<const Eigen::Matrix<double, 9, 1>> estimate =
+        this->getEstimate();
+    return SO3::unflatten(estimate);
+  }
+
+  // The plus operator
+  virtual void plus(const double *x, const double *delta_xi,
+                    double *x_plus_delta_xi) const override final {
+    local_parameterization_ptr_->Plus(x, delta_xi, x_plus_delta_xi);
+  }
+
+  // Plus Jacobian
+  virtual void plusJacobian(const double *x0,
+                            double *jacobian) const override final {
+    local_parameterization_ptr_->ComputeJacobian(x0, jacobian);
+  }
+};
